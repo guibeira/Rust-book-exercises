e61ba7dc30750f3c3f9d932644e3caedc59ff843
GuilhermeVBeira <guilherme.vieira.beira@gmail.com>
2020-07-27 22:35:23 -0300

    Defining a var

--- 3. Common Programming Concepts/3.1. Variables and Mutability/variables/src/main.rs
+++ 3. Common Programming Concepts/3.1. Variables and Mutability/variables/src/main.rs
@@ -1,6 +1,19 @@
 fn main() {
-    let mut x = 5;
+    let mut x = 5; // using mut you can change the value
     println!("The value of x  is: {}", x);
     x = 6;
     println!("The value of x  is: {}", x);
+
+    //shadowing
+    let x = 5;
+    let x = x + 1;
+    let x = x * 2;
+
+    println!("The value of x is: {}", x);
+
+    let spaces = "   ";
+    let spaces = spaces.len(); // creating new var and changing his type
+   
+    // let mut spaces = "    ";
+    // spaces = spaces.len(); // error because you are trying to change the type
 }
