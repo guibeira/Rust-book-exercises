74f99a778fcd575b2d0e0996107d77877ca72dda
GuilhermeVBeira <guilherme.vieira.beira@gmail.com>
2020-07-31 18:24:53 -0300

    Functions

---
+++ 3. Common Programming Concepts/3.3. Functions/functions/functions/src/main.rs
@@ -0,0 +1,31 @@
+fn main() {
+    println!("Hello, world!");
+
+    another_function_with_parameters(5, 6);
+
+
+    let x = 5;
+
+    let y = {
+        let x = 3;
+        x + 1
+    };
+
+    println!("The value of y is: {}", y);
+    
+    let returned_five = five();
+    println!("The value of returned_five is: {}", returned_five)
+}
+
+fn another_function() {
+    println!("Another function.");
+}
+
+fn another_function_with_parameters(x: i32, y: i32) {
+    println!("The value of x is: {}", x);
+    println!("The value of y is: {}", y);
+}
+
+fn five() -> i32 {
+    5
+}
