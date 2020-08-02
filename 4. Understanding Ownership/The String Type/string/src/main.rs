b4f91855a47088e38418b3f36e8b090d9fad70a4
GuilhermeVBeira <guilherme.vieira.beira@gmail.com>
2020-08-02 16:45:10 -0300

    Return Values and Scope

--- 4. Understanding Ownership/The String Type/string/src/main.rs
+++ 4. Understanding Ownership/The String Type/string/src/main.rs
@@ -1,31 +1,18 @@
-fn main() {
-    let mut s = String::from("hello");
-    s.push_str(", world!");
-    println!("{}", s);
+fn main(){
+    let s1 = gives_ownership;
 
-    // Clone data
+    let s2 = String::from("hello");
 
-    let s1 = String::from("hello");
-    let s2 = s1.clone();
-
-    println!("s1 = {}, s2 = {}", s1, s2);
-
-    let x = 5;
-    let y = x;
-
-    println!("x = {}, y = {}", x, y);
-
-    let s = String::from("hello");
-    takes_ownership(s);
-
-    let x = 5;
-    makes_copy(x);
+    let s3 = takes_and_gives_back(s2);
+    
 }
 
-fn takes_ownership(some_string: String) {
-    println!("{}", some_string);
+fn gives_ownership() -> String {
+    let some_string = String::from("hello");
+
+    some_string
 }
 
-fn makes_copy(some_integer: i32) {
-    println!("{}", some_integer);
+fn takes_and_gives_back(a_string: String) -> String {
+    a_string
 }
