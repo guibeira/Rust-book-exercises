88fce0eb8f19f1b389faea130978aceae586164a
GuilhermeVBeira <guilherme.vieira.beira@gmail.com>
2020-08-02 16:48:09 -0300

    Returning ownership of parameters

--- 4. Understanding Ownership/The String Type/string/src/main.rs
+++ 4. Understanding Ownership/The String Type/string/src/main.rs
@@ -1,18 +1,14 @@
-fn main(){
-    let s1 = gives_ownership;
+fn main() {
+    let s1 = String::from("hello");
 
-    let s2 = String::from("hello");
+    let (s2, len) = calculate_length(s1);
 
-    let s3 = takes_and_gives_back(s2);
-    
+    println!("The length of '{}' is {}", s2, len);
+
 }
 
-fn gives_ownership() -> String {
-    let some_string = String::from("hello");
+fn calculate_length(s: String) -> (String, usize) {
+    let length = s.len();
 
-    some_string
-}
-
-fn takes_and_gives_back(a_string: String) -> String {
-    a_string
+    (s, length)
 }
