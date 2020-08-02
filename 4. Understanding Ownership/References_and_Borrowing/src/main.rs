63dbc5bcdba39cae4530271292db0144c05d3638
GuilhermeVBeira <guilherme.vieira.beira@gmail.com>
2020-08-02 18:31:22 -0300

    Only one reference on the same scope

--- 4. Understanding Ownership/References_and_Borrowing/src/main.rs
+++ 4. Understanding Ownership/References_and_Borrowing/src/main.rs
@@ -1,9 +1,9 @@
 fn main() {
     let mut s = String::from("hello");
 
-    change(&mut s);
+    let r1 = &mut s;
+    let r2 = &mut s;
+
+    println!("{}, {}", r1, r2);
 }
 
-fn change(some_string: &mut String) {
-    some_string.push_str(", world");
-}
