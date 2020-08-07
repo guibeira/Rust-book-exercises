001f3896b3ed4a7f57b89b9c31d9422d771da2d0
GuilhermeVBeira <guilherme.vieira.beira@gmail.com>
2020-08-06 21:35:27 -0300

    Example using tuple

--- 5. Using Structs to Structure Related Data/an_example_program_using_structs/src/main.rs
+++ 5. Using Structs to Structure Related Data/an_example_program_using_structs/src/main.rs
@@ -1,13 +1,13 @@
 fn main() {
-    let width = 30;
-    let height = 50;
+    
+    let rect1 = (30, 50);
 
     println!("The area of the rectangle  is {} square pixels.",
-             area(width, height)
+             area(rect1)
     );
 }
 
 
-fn area(width: u32, height: u32) -> u32 {
-    width * height
+fn area(dimensions: (u32, u32)) -> u32 {
+    dimensions.0 * dimensions.1
 }
