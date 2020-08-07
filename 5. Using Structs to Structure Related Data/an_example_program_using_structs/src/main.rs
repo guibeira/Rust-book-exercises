b1ba6428796c8de7680b48e73d77b0d2d0bbe1ba
GuilhermeVBeira <guilherme.vieira.beira@gmail.com>
2020-08-06 21:45:55 -0300

    Print element using debug

--- 5. Using Structs to Structure Related Data/an_example_program_using_structs/src/main.rs
+++ 5. Using Structs to Structure Related Data/an_example_program_using_structs/src/main.rs
@@ -1,3 +1,5 @@
+#[derive(Debug)]
+
 struct Rectangle {
     width: u32,
     height: u32,
@@ -6,7 +8,8 @@ struct Rectangle {
 fn main() {
     let rect1 = Rectangle { width: 30, height: 50};
     
-    
+    println!("rect1 is {:#?}", rect1);
+
     println!("The area of the rectangle  is {} square pixels.",
              area(&rect1)
     );
