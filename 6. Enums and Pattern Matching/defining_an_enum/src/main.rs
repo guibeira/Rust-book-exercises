0f18c5726a1c929e6b0dd3159b53126570020c41
GuilhermeVBeira <guilherme.vieira.beira@gmail.com>
2020-08-10 11:20:20 -0300

    Using diferents types

--- 6. Enums and Pattern Matching/defining_an_enum/src/main.rs
+++ 6. Enums and Pattern Matching/defining_an_enum/src/main.rs
@@ -1,9 +1,9 @@
 fn main() {
     enum IpAddr {
-        V4(String),
+        V4(u8,  u8, u8, u8),
         V6(String),
     }
 
-    let home = IpAddr::V4(String::from("127.0.0.1"));    
+    let home = IpAddr::V4(127, 0, 0, 1);    
     let loopback = IpAddr::V6(String::from("::1")); 
 }
