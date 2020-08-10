ae13d01571750cc49ea613c832a3098efe3eb429
GuilhermeVBeira <guilherme.vieira.beira@gmail.com>
2020-08-10 15:42:31 -0300

    Run code inside patter match

--- 6. Enums and Pattern Matching/the_match_control_flow_operator/src/main.rs
+++ 6. Enums and Pattern Matching/the_match_control_flow_operator/src/main.rs
@@ -7,7 +7,10 @@ enum Coin {
 
 fn value_in_cents(coin: Coin) -> u8 {
     match coin {
-        Coin::Penny => 1,
+        Coin::Penny => {
+            println!("Lucky penny!"); // every time he show "Lucky penny!"
+            1
+        },
         Coin::Nickel => 5,
         Coin::Dime => 10,
         Coin::Quarter => 25,
