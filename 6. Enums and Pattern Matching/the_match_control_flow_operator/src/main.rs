8c3d2d95c45068a06696f58ddcfe4ea81026c363
GuilhermeVBeira <guilherme.vieira.beira@gmail.com>
2020-08-10 16:11:13 -0300

    match with option

--- 6. Enums and Pattern Matching/the_match_control_flow_operator/src/main.rs
+++ 6. Enums and Pattern Matching/the_match_control_flow_operator/src/main.rs
@@ -1,22 +1,12 @@
-enum Coin {
-    Penny,
-    Nickel,
-    Dime,
-    Quarter,
-}
-
-fn value_in_cents(coin: Coin) -> u8 {
-    match coin {
-        Coin::Penny => {
-            println!("Lucky penny!"); // every time he show "Lucky penny!"
-            1
-        },
-        Coin::Nickel => 5,
-        Coin::Dime => 10,
-        Coin::Quarter => 25,
-    }
-}
-
 fn main() {
+    fn plus_one(x: Option<i32>) -> Option<i32> {
+        match x {
+            None => None,
+            Some(i) => Some(i + 1),
+        }
+    }
 
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
 }
