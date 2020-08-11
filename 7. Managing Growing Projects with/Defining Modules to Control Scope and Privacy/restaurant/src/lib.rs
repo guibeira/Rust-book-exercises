4742ed0a427c5bd1961378a6a682b4f7f03c1152
GuilhermeVBeira <guilherme.vieira.beira@gmail.com>
2020-08-10 22:26:29 -0300

    Using pub enum

--- 7. Managing Growing Projects with/Defining Modules to Control Scope and Privacy/restaurant/src/lib.rs
+++ 7. Managing Growing Projects with/Defining Modules to Control Scope and Privacy/restaurant/src/lib.rs
@@ -17,25 +17,12 @@ mod front_of_house {
 fn serve_order() {}
 
 mod back_of_house {
-    pub struct Breakfast {
-        pub toast: String,
-        seasonal_fruit: String,
+    pub enum Appetizer {
+        Soup,
+        Salad,
     }
-
-    impl Breakfast {
-        pub fn summer(toast: &str) -> Breakfast {
-            Breakfast {
-                toast: String::from(toast),
-                seasonal_fruit: String::from("peaches"),
-            }
-        }
-    }
-}
 
 pub fn eat_at_restaurant() {
-    let mut meal = back_of_house::Breakfast::summer("Rye");
-
-    meal.toast = String::from("Wheat");
-    println!("I'd like {} toast please", meal.toast);
-
+    let order1 = back_of_house::Appetizer::Soup;
+    let order2 = back_of_house::Appetizer::Salad;
 }
