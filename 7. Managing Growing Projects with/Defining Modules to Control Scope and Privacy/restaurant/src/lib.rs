8bdf9a3f0a5791725fb28b65ed04da1c0a23ae25
GuilhermeVBeira <guilherme.vieira.beira@gmail.com>
2020-08-10 22:23:54 -0300

    Using pub struct

--- 7. Managing Growing Projects with/Defining Modules to Control Scope and Privacy/restaurant/src/lib.rs
+++ 7. Managing Growing Projects with/Defining Modules to Control Scope and Privacy/restaurant/src/lib.rs
@@ -14,21 +14,28 @@ mod front_of_house {
     }
 }
 
-pub fn eat_at_restaurant() {
-    // absolute path
-    crate::front_of_house::hosting::add_to_waitlist();
-
-    // relative path
-    front_of_house::hosting::add_to_waitlist();
-}
-
 fn serve_order() {}
 
 mod back_of_house {
-   fn fix_incorrect_order() {
-        cook_order();
-        super::serve_order();
-   } 
-   
-   fn cook_order() {}
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
+    }
+}
+
+pub fn eat_at_restaurant() {
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+
+    meal.toast = String::from("Wheat");
+    println!("I'd like {} toast please", meal.toast);
+
 }
