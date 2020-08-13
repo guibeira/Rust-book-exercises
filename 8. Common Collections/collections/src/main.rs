f4d37d8c8013731685b34a9b92ec3285e7c2f28a
GuilhermeVBeira <gui_doggao@hotmail.com>
2020-08-13 17:04:06 -0300

    Updating strings

--- 8. Common Collections/collections/src/main.rs
+++ 8. Common Collections/collections/src/main.rs
@@ -6,4 +6,10 @@ fn main(){
     let s = "Initial contents".to_string();
 
     let s = String::from("Initial contests");
+
+    let mut s1 = String::from("foo");
+
+    s1.push_str("bar");
+
+    println!("s1 is {}", s1);
 }
