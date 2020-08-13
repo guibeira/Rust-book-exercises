cc6047b478bdd7b997373b0d51b1e5e9b09a35aa
GuilhermeVBeira <gui_doggao@hotmail.com>
2020-08-12 23:42:54 -0300

    Use 'use' in stand lib

--- 7. Managing Growing Projects with/Defining Modules to Control Scope and Privacy/restaurant/src/lib.rs
+++ 7. Managing Growing Projects with/Defining Modules to Control Scope and Privacy/restaurant/src/lib.rs
@@ -1,16 +1,8 @@
-mod front_of_house {
-    pub mod hosting {
-        pub fn add_to_waitlist() {}
-    }
+use std::collections::HashMap;
+
+fn main() {
+    let mut map = HashMap::new();
+    map.insert(1, 2);
+
 }
-
-use crate::front_of_house::hosting;
-
-pub fn eat_at_restaurant() {
-    hosting::add_to_waitlist();
-    hosting::add_to_waitlist();
-    hosting::add_to_waitlist();
-}
-
-fn main() {}
 
