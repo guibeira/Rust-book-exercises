27b27c84d62e07c85eb224782e0379c4c24e6c0d
GuilhermeVBeira <gui_doggao@hotmail.com>
2020-08-12 23:47:06 -0300

    Use 'as' on import

--- 7. Managing Growing Projects with/Defining Modules to Control Scope and Privacy/restaurant/src/lib.rs
+++ 7. Managing Growing Projects with/Defining Modules to Control Scope and Privacy/restaurant/src/lib.rs
@@ -1,8 +1,19 @@
-use std::collections::HashMap;
-
+#![allow(unused_variables)]
 fn main() {
-    let mut map = HashMap::new();
-    map.insert(1, 2);
+    use std::fmt::Result;
+    use std::io::Result as IoResult;
 
+    fn function1() -> Result {
+            // --snip--
+            //     Ok(())
+            //     
+    }
+
+    fn function2() -> IoResult<()> {
+            // --snip--
+            //     Ok(())
+            //     
+    }
+    
 }
 
