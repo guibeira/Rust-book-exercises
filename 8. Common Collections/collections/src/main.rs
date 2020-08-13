263384b46606c5d61da0ce2cfdf6fb44063aa78c
GuilhermeVBeira <gui_doggao@hotmail.com>
2020-08-13 12:01:11 -0300

    Using enum on vector

--- 8. Common Collections/collections/src/main.rs
+++ 8. Common Collections/collections/src/main.rs
@@ -1,13 +1,13 @@
-fn main() {
-    let mut v = vec![100, 32, 57];
-    for i in &mut v {
-        *i += 50;
-
+fn main(){
+    enum SpreadsheetCell {
+        Int(i32),
+        Float(f64),
+        Text(String),
     }
-    for i in &v {
-        println!("{}", i);
-
-    }
-
+    
+    let row = vec![
+        SpreadsheetCell::Int(3),
+        SpreadsheetCell::Text(String::from("Blue")),
+        SpreadsheetCell::Float(10.12),
+    ];
 }
-
