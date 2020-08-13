17865a448bae268dc8efd5c4088bc7fd8a6aa6a6
GuilhermeVBeira <gui_doggao@hotmail.com>
2020-08-13 18:21:31 -0300

    Using zip to create hashmap

--- 8. Common Collections/collections/src/main.rs
+++ 8. Common Collections/collections/src/main.rs
@@ -1,10 +1,10 @@
 fn main() {
     use std::collections::HashMap;
+    
+    let teams = vec!["Blue".to_string(), "Yellow".to_string()];
+    let initial_scores = vec![10, 50];
 
-    let mut scores = HashMap::new();
-
-    scores.insert(String::from("Blue"), 10);
-    scores.insert(String::from("Yellow"), 50);
+    let mut scores: HashMap<_, _> = teams.into_iter().zip(initial_scores.into_iter()).collect();
 
 }
 
