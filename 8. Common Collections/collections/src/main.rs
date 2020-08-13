6c00cb4b5b61b3f4c9ee4296f502fb9d55b841a9
GuilhermeVBeira <gui_doggao@hotmail.com>
2020-08-13 11:37:05 -0300

    Reading elements from vec

--- 8. Common Collections/collections/src/main.rs
+++ 8. Common Collections/collections/src/main.rs
@@ -1,10 +1,11 @@
-fn main() {
-    let mut v = Vec::new();
+fn main(){
+    let v = vec![1, 2, 3, 4, 5];
 
-    v.push(5);
-    v.push(6);
-    v.push(7);
-    v.push(8);
+    let third: &i32 = &v[2];
+    println!("The third element is {}", third);
 
+    match v.get(2) {
+        Some(third) => println!("the third element is {}", third),
+        None => println!("There is no third element"),
+    }
 }
-
