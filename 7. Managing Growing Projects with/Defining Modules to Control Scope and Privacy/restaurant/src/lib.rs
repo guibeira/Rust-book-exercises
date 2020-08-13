1e35507d59ffb892f0519ef719dfd09a9c365e6d
GuilhermeVBeira <guilherme.vieira.beira@gmail.com>
2020-08-12 22:54:40 -0300

    Introductions to "use"

--- 7. Managing Growing Projects with/Defining Modules to Control Scope and Privacy/restaurant/src/lib.rs
+++ 7. Managing Growing Projects with/Defining Modules to Control Scope and Privacy/restaurant/src/lib.rs
@@ -1,28 +1,16 @@
 mod front_of_house {
     pub mod hosting {
         pub fn add_to_waitlist() {}
-
-        fn seat_at_table() {}
-    }
-
-    mod serving {
-        fn take_order() {}
-
-        fn serve_order() {}
-
-        fn take_payment() {}
     }
 }
 
-fn serve_order() {}
-
-mod back_of_house {
-    pub enum Appetizer {
-        Soup,
-        Salad,
-    }
+use crate::front_of_house::hosting;
 
 pub fn eat_at_restaurant() {
-    let order1 = back_of_house::Appetizer::Soup;
-    let order2 = back_of_house::Appetizer::Salad;
+    hosting::add_to_waitlist();
+    hosting::add_to_waitlist();
+    hosting::add_to_waitlist();
 }
+
+fn main() {}
+
