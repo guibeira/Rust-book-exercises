4848bcbacf7e4906275853f5879eb455ff7ad761
GuilhermeVBeira <gui_doggao@hotmail.com>
2020-08-13 23:12:02 -0300

    Use entry method to update hashmap

--- 8. Common Collections/collections/src/main.rs
+++ 8. Common Collections/collections/src/main.rs
@@ -1,16 +1,13 @@
 fn main() {
     use std::collections::HashMap;
-    
-    let teams = vec!["Blue".to_string(), "Yellow".to_string()];
-    let initial_scores = vec![10, 50];
 
-    let mut scores: HashMap<_, _> = teams.into_iter().zip(initial_scores.into_iter()).collect();
+    let mut scores = HashMap::new();
+    scores.insert(String::from("Blue"), 10);
 
-    let score_blue_team = scores.get(&"Blue".to_string());
-    for (key, value) in &scores {
-        println!("{}: {}", key, value);
+    scores.entry(String::from("Yellow")).or_insert(50);
+    scores.entry(String::from("Blue")).or_insert(50);
 
-    }
+    println!("{:?}", scores);
 
 }
 
