dc413b3e747a6c41e983b5bc7c032dba859daa80
GuilhermeVBeira <gui_doggao@hotmail.com>
2020-08-13 23:21:42 -0300

    Updating a Value Based on the Old Value

--- 8. Common Collections/collections/src/main.rs
+++ 8. Common Collections/collections/src/main.rs
@@ -1,13 +1,17 @@
 fn main() {
     use std::collections::HashMap;
 
-    let mut scores = HashMap::new();
-    scores.insert(String::from("Blue"), 10);
+    let text = "hello world wonderful world";
 
-    scores.entry(String::from("Yellow")).or_insert(50);
-    scores.entry(String::from("Blue")).or_insert(50);
+    let mut map = HashMap::new();
 
-    println!("{:?}", scores);
+    for word in text.split_whitespace() {
+        let count = map.entry(word).or_insert(0);
+        *count += 1;
+
+    }
+
+    println!("{:?}", map);
 
 }
 
