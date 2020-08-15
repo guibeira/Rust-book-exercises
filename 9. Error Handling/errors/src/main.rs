76ec961d36dbdeeef75093c8d0805469dd710bb3
GuilhermeVBeira <guilherme.vieira.beira@gmail.com>
2020-08-14 22:05:26 -0300

    The ? operator

--- 9. Error Handling/errors/src/main.rs
+++ 9. Error Handling/errors/src/main.rs
@@ -1,6 +1,14 @@
-use std::fs::File;
-
+#![allow(unused_variables)]
 fn main() {
-    let f = File::open("hello.txt").expect("Deu ruim ao abrir o arquivo");
+	use std::fs::File;
+	use std::io;
+	use std::io::Read;
+
+	fn read_username_from_file() -> Result<String, io::Error> {
+		let mut f = File::open("hello.txt")?;
+		let mut s = String::new();
+		f.read_to_string(&mut s)?;
+		Ok(s)
+	}
 }
 
