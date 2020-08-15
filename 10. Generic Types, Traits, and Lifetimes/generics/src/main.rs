00c5aeee62590b3a5407746b47af201ec307cbbf
GuilhermeVBeira <gui_doggao@hotmail.com>
2020-08-15 13:53:26 -0300

    Implement trait like interface

--- 10. Generic Types, Traits, and Lifetimes/generics/src/main.rs
+++ 10. Generic Types, Traits, and Lifetimes/generics/src/main.rs
@@ -1,34 +1,34 @@
-
 #![allow(unused_variables)]
 fn main() {
-pub trait Summary {
-    fn summarize(&self) -> String;
-}
+    pub trait Summary {
+	fn summarize_author(&self) -> String;
 
-pub struct NewsArticle {
-    pub headline: String,
-    pub location: String,
-    pub author: String,
-    pub content: String,
-}
-
-impl Summary for NewsArticle {
-    fn summarize(&self) -> String {
-        format!("{}, by {} ({})", self.headline, self.author, self.location)
+	fn summarize(&self) -> String {
+	    format!("(Read more from {}...)", self.summarize_author())
+	}
     }
-}
 
-pub struct Tweet {
-    pub username: String,
-    pub content: String,
-    pub reply: bool,
-    pub retweet: bool,
-}
-
-impl Summary for Tweet {
-    fn summarize(&self) -> String {
-        format!("{}: {}", self.username, self.content)
+    pub struct Tweet {
+	pub username: String,
+	pub content: String,
+	pub reply: bool,
+	pub retweet: bool,
     }
-}
+
+    impl Summary for Tweet {
+	fn summarize_author(&self) -> String {
+	    format!("@{}", self.username)
+	}
+    }
+    let tweet = Tweet {
+        username: String::from("horse_ebooks"),
+        content: String::from(
+            "of course, as you probably already know, people",
+        ),
+        reply: false,
+        retweet: false,
+    };
+
+    println!("1 new tweet: {}", tweet.summarize());
 }
 
