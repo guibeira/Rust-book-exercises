047633fb3929882a588c94980121d8c9bb2722a2
GuilhermeVBeira <guilherme.vieira.beira@gmail.com>
2020-08-14 21:36:13 -0300

    Handling with many kinds erros

--- 9. Error Handling/errors/src/main.rs
+++ 9. Error Handling/errors/src/main.rs
@@ -1,11 +1,20 @@
 use std::fs::File;
+use std::io::ErrorKind;
 
 fn main() {
     let f = File::open("hello.txt");
-    
+
     let f = match f {
         Ok(file) => file,
-        Err(error) => panic!("Problem opening the file: {:?}", error),
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => match File::create("hello.txt") {
+                Ok(fc) => fc,
+                Err(e) => panic!("Problem creating the file: {:?}", e),
+            },
+            other_error => {
+                panic!("Problem opening the file: {:?}", other_error)
+            }
+        },
     };
 }
 
