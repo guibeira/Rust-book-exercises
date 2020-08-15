3ea59d83704d0aa54bcfe7259f3217b2e215161b
GuilhermeVBeira <gui_doggao@hotmail.com>
2020-08-15 13:32:51 -0300

    Introduction to trait

--- 10. Generic Types, Traits, and Lifetimes/generics/src/main.rs
+++ 10. Generic Types, Traits, and Lifetimes/generics/src/main.rs
@@ -1,22 +1,34 @@
-struct Point<T, U> {
-    x: T,
-    y: U,
+
+#![allow(unused_variables)]
+fn main() {
+pub trait Summary {
+    fn summarize(&self) -> String;
 }
 
-impl<T, U> Point<T, U> {
-    fn mixup<V, W>(self, other: Point<V, W>) -> Point<T, W> {
-        Point {
-            x: self.x,
-            y: other.y,
-        }
+pub struct NewsArticle {
+    pub headline: String,
+    pub location: String,
+    pub author: String,
+    pub content: String,
+}
+
+impl Summary for NewsArticle {
+    fn summarize(&self) -> String {
+        format!("{}, by {} ({})", self.headline, self.author, self.location)
     }
 }
 
-fn main() {
-    let p1 = Point { x: 5, y: 10.4 };
-    let p2 = Point { x: "Hello", y: 'c' };
-
-    let p3 = p1.mixup(p2);
-
-    println!("p3.x = {}, p3.y = {}", p3.x, p3.y);
+pub struct Tweet {
+    pub username: String,
+    pub content: String,
+    pub reply: bool,
+    pub retweet: bool,
 }
+
+impl Summary for Tweet {
+    fn summarize(&self) -> String {
+        format!("{}: {}", self.username, self.content)
+    }
+}
+}
+
