014b048c34e1e90a484d830485945db1f3f3ba57
GuilhermeVBeira <guilherme.vieira.beira@gmail.com>
2020-08-18 22:00:06 -0300

    Struct holds reference

--- 10. Generic Types, Traits, and Lifetimes/generics/src/main.rs
+++ 10. Generic Types, Traits, and Lifetimes/generics/src/main.rs
@@ -1,16 +1,12 @@
-fn main() {
-    let string1 = String::from("abcd");
-    let string2 = "xyz";
-
-    let result = longest(string1.as_str(), string2);
-    println!("The longest string is {}", result);
+struct ImportantExcerpt<'a> {
+    part: &'a str,
 }
 
-fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
-    if x.len() > y.len() {
-        x
-    } else {
-        y
-    }
+fn main() {
+    let novel = String::from("Call me Ishmael. Some years ago...");
+    let first_sentence = novel.split('.').next().expect("Could not find a '.'");
+    let i = ImportantExcerpt {
+        part: first_sentence,
+    };
 }
 
