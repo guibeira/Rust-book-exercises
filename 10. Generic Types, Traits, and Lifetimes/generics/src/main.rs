0760376b2ea3871df1560c8156d9a11ab9c1d48a
GuilhermeVBeira <guilherme.vieira.beira@gmail.com>
2020-08-18 22:23:19 -0300

    Generic Type Parameters, Trait Bounds, and Lifetimes Together

--- 10. Generic Types, Traits, and Lifetimes/generics/src/main.rs
+++ 10. Generic Types, Traits, and Lifetimes/generics/src/main.rs
@@ -1,12 +1,30 @@
-struct ImportantExcerpt<'a> {
-    part: &'a str,
+fn main() {
+    let string1 = String::from("abcd");
+    let string2 = "xyz";
+
+    let result = longest_with_an_announcement(
+        string1.as_str(),
+        string2,
+        "Today is someone's birthday!",
+    );
+    println!("The longest string is {}", result);
 }
 
-fn main() {
-    let novel = String::from("Call me Ishmael. Some years ago...");
-    let first_sentence = novel.split('.').next().expect("Could not find a '.'");
-    let i = ImportantExcerpt {
-        part: first_sentence,
-    };
+use std::fmt::Display;
+
+fn longest_with_an_announcement<'a, T>(
+    x: &'a str,
+    y: &'a str,
+    ann: T,
+) -> &'a str
+where
+    T: Display,
+{
+    println!("Announcement! {}", ann);
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
 }
 
