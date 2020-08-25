c52c62d45ff0514f3ad98505272fa442e9b2d5d6
GuilhermeVBeira <guilherme.vieira.beira@gmail.com>
2020-08-24 21:50:14 -0300

    Custom failure messages

--- 11. Writing Automated Tests/adder/src/lib.rs
+++ 11. Writing Automated Tests/adder/src/lib.rs
@@ -1,48 +1,18 @@
-#[derive(Debug)]
-struct Rectangle {
-    width: u32,
-    height: u32,
-}
-
-// --snip--
-impl Rectangle {
-    fn can_hold(&self, other: &Rectangle) -> bool {
-        self.width < other.width && self.height > other.height
-    }
+pub fn greeting(name: &str) -> String {
+    format!("Hello !")
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-
+    
     #[test]
-    fn larger_can_hold_smaller() {
-        let larger = Rectangle {
-            width: 8,
-            height: 7,
-        };
-        let smaller = Rectangle {
-            width: 5,
-            height: 1,
-        };
-
-        assert!(larger.can_hold(&smaller));
-    }
-
-    #[test]
-    fn smaller_cannot_hold_larger() {
-        let larger = Rectangle {
-            width: 8,
-            height: 7,
-        };
-        let smaller = Rectangle {
-            width: 5,
-            height: 1,
-        };
-
-        assert!(!smaller.can_hold(&larger));
+    fn greeting_contains_name(){
+        let result = greeting("Carol");
+        assert!(
+            result.contains("Carol"),
+            "Greeting did not contain name, value was `{}`",
+            result
+        );
     }
 }
-
-fn main() {}
-
