9536a781b547129d48c375a9e395073c2900c4c6
GuilhermeVBeira <guilherme.vieira.beira@gmail.com>
2020-08-24 22:33:31 -0300

    Show output tests

--- 11. Writing Automated Tests/adder/src/lib.rs
+++ 11. Writing Automated Tests/adder/src/lib.rs
@@ -1,18 +1,6 @@
-pub struct Guess {
-    value: i32,
-}
-
-impl Guess {
-    pub fn new(value: i32) -> Guess {
-        if value < 1 {
-            panic!("Guess value must be greater than or equal 1, got {}.", value);
-
-        } else if value > 100 {
-            panic!("Guess value must be less than or equal 100, got {}", value);
-        }
-
-        Guess { value }
-    }
+fn prints_and_returns_10(a: i32) -> i32 {
+    println!("I got the  value {}", a);
+    10
 }
 
 #[cfg(test)]
@@ -20,8 +8,14 @@ mod tests {
     use super::*;
 
     #[test]
-    #[should_panic(expected = "Guess value must be less than or equal 100, got 200")]
-    fn greater_than_100(){
-        Guess::new(200);
+    fn this_test_will_pass(){
+        let value = prints_and_returns_10(4);
+        assert_eq!(10, value);
+    }
+
+    #[test]
+    fn this_test_will_fail(){
+        let value = prints_and_returns_10(8);
+        assert_eq!(5, value)
     }
 }
