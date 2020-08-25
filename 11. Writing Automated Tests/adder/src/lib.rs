46a38c106b312a20646eb0de7b5d7d78caff58d9
GuilhermeVBeira <guilherme.vieira.beira@gmail.com>
2020-08-24 22:06:25 -0300

    Add should panic on tests

--- 11. Writing Automated Tests/adder/src/lib.rs
+++ 11. Writing Automated Tests/adder/src/lib.rs
@@ -1,18 +1,24 @@
-pub fn greeting(name: &str) -> String {
-    format!("Hello !")
+pub struct Guess {
+    value: i32,
+}
+
+impl Guess {
+    pub fn new(value: i32) -> Guess {
+        if value < 1 || value > 100 {
+            panic!("Guess value must be between 1 and 100, got {}.", value);
+        }
+
+        Guess { value }
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    
+
     #[test]
-    fn greeting_contains_name(){
-        let result = greeting("Carol");
-        assert!(
-            result.contains("Carol"),
-            "Greeting did not contain name, value was `{}`",
-            result
-        );
+    #[should_panic]
+    fn greater_than_100(){
+        Guess::new(200);
     }
 }
