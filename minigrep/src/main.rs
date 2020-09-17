9de67a93819c93545e5d09810c33dbbdbffa6d5f
GuilhermeVBeira <guilherme.vieira.beira@gmail.com>
2020-09-17 20:41:17 -0300

    Add command line tool

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,29 +1,20 @@
+use std::process;
 use std::env;
-use std::fs;
+use minigrep::Config;
 
 fn main() {
     // --snip--
     let args: Vec<String> = env::args().collect();
 
-    let config = parse_config(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.filename);
+    if let Err(e) = minigrep::run(config){
+        println!("Application error: {}", e);
+        process::exit(1);
+    }
 
-    let contents = fs::read_to_string(config.filename)
-        .expect("Something went wrong reading the file");
-
-    println!("With text:\n{}", contents);
 }
 
-struct Config {
-    query: String,
-    filename: String,
-}
-
-fn parse_config(args: &[String]) -> Config {
-    let query = args[1].clone();
-    let filename = args[2].clone();
-
-    Config { query, filename }
-}
