1348868b85cb816f640d4b15d832b01ac0fec45d
GuilhermeVBeira <gui_doggao@hotmail.com>
2020-10-27 21:07:14 -0300

    Closures

--- 13. Functional Language Features: Iterators and Closures/closures/src/main.rs
+++ 13. Functional Language Features: Iterators and Closures/closures/src/main.rs
@@ -56,7 +56,7 @@ fn generate_workout(intensity: u32, random_number: u32) {
 
 fn main() {
     let simulated_user_specified_value = 10;
-    let simulated_random_number = 7;
+    let simulated_random_number =   7;
 
     generate_workout(simulated_user_specified_value, simulated_random_number);
 }
