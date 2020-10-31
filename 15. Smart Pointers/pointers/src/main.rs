6daeef30d6a2a44147ecf973fcd00cda3b5a96c5
GuilhermeVBeira <guilherme.vieira.beira@gmail.com>
2020-10-31 13:54:49 -0300

    Leak Memory

--- 15. Smart Pointers/pointers/src/main.rs
+++ 15. Smart Pointers/pointers/src/main.rs
@@ -1,24 +1,42 @@
-#[derive(Debug)]
-enum List {
-    Cons(Rc<RefCell<i32>>, Rc<List>),
-    Nil,
-}
-
 use crate::List::{Cons, Nil};
 use std::cell::RefCell;
 use std::rc::Rc;
 
+#[derive(Debug)]
+enum List {
+    Cons(i32, RefCell<Rc<List>>),
+    Nil,
+}
+
+impl List {
+    fn tail(&self) -> Option<&RefCell<Rc<List>>> {
+        match self {
+            Cons(_, item) => Some(item),
+            Nil => None,
+        }
+    }
+}
+
 fn main() {
-    let value = Rc::new(RefCell::new(5));
+    let a = Rc::new(Cons(5, RefCell::new(Rc::new(Nil))));
 
-    let a = Rc::new(Cons(Rc::clone(&value), Rc::new(Nil)));
+    println!("a initial rc count = {}", Rc::strong_count(&a));
+    println!("a next item = {:?}", a.tail());
 
-    let b = Cons(Rc::new(RefCell::new(3)), Rc::clone(&a));
-    let c = Cons(Rc::new(RefCell::new(4)), Rc::clone(&a));
+    let b = Rc::new(Cons(10, RefCell::new(Rc::clone(&a))));
 
-    *value.borrow_mut() += 10;
+    println!("a rc count after b creation = {}", Rc::strong_count(&a));
+    println!("b initial rc count = {}", Rc::strong_count(&b));
+    println!("b next item = {:?}", b.tail());
 
-    println!("a after = {:?}", a);
-    println!("b after = {:?}", b);
-    println!("c after = {:?}", c);
+    if let Some(link) = a.tail() {
+        *link.borrow_mut() = Rc::clone(&b);
+    }
+
+    println!("b rc count after changing a = {}", Rc::strong_count(&b));
+    println!("a rc count after changing a = {}", Rc::strong_count(&a));
+
+    // Uncomment the next line to see that we have a cycle;
+    // it will overflow the stack
+    // println!("a next item = {:?}", a.tail());
 }
