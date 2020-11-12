c98006c0f02382cb524f8c2635a73134b7ae2776
GuilhermeVBeira <guilherme.vieira.beira@gmail.com>
2020-11-12 10:45:30 -0300

    Use atomic

--- 16. Fearless Concurrency/threads/src/main.rs
+++ 16. Fearless Concurrency/threads/src/main.rs
@@ -1,44 +1,23 @@
-use std::sync::mpsc;
+use std::sync::{Arc, Mutex};
 use std::thread;
-use std::time::Duration;
 
 fn main() {
-    // --snip--
+    let counter = Arc::new(Mutex::new(0));
+    let mut handles = vec![];
 
-    let (tx, rx) = mpsc::channel();
+    for _ in 0..10 {
+        let counter = Arc::clone(&counter);
+        let handle = thread::spawn(move || {
+            let mut num = counter.lock().unwrap();
 
-    let tx1 = mpsc::Sender::clone(&tx);
-    thread::spawn(move || {
-        let vals = vec![
-            String::from("hi"),
-            String::from("from"),
-            String::from("the"),
-            String::from("thread"),
-        ];
-
-        for val in vals {
-            tx1.send(val).unwrap();
-            thread::sleep(Duration::from_secs(1));
-        }
-    });
-
-    thread::spawn(move || {
-        let vals = vec![
-            String::from("more"),
-            String::from("messages"),
-            String::from("for"),
-            String::from("you"),
-        ];
-
-        for val in vals {
-            tx.send(val).unwrap();
-            thread::sleep(Duration::from_secs(1));
-        }
-    });
-
-    for received in rx {
-        println!("Got: {}", received);
+            *num += 1;
+        });
+        handles.push(handle);
     }
 
-    // --snip--
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    println!("Result: {}", *counter.lock().unwrap());
 }
