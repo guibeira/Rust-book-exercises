1af84b3d5ab6568ec7e6f6a780e6cb2c1733fd47
GuilhermeVBeira <guilherme.vieira.beira@gmail.com>
2020-11-13 15:46:32 -0300

    State pattern

---
+++ 17. Object Oriented Programming Features of Rust/oo/src/lib.rs
@@ -0,0 +1,26 @@
+pub struct Post {
+    state: Option<Box<dyn State>>,
+    content: String,
+}
+
+impl Post {
+    // --snip--
+    pub fn new() -> Post {
+        Post {
+            state: Some(Box::new(Draft {})),
+            content: String::new(),
+        }
+    }
+
+    pub fn add_text(&mut self, text: &str) {
+        self.content.push_str(text);
+    }
+}
+
+trait State {}
+
+struct Draft {}
+
+impl State for Draft {}
+
+fn main() {}
--- 17. Object Oriented Programming Features of Rust/oo/src/main.rs
+++ 17. Object Oriented Programming Features of Rust/oo/src/main.rs
@@ -1,63 +1,90 @@
-pub trait Draw {
-    fn draw(&self);
+
+#![allow(unused)]
+fn main() {
+pub struct Post {
+    state: Option<Box<dyn State>>,
+    content: String,
 }
 
-pub struct Screen {
-    pub components: Vec<Box<dyn Draw>>,
-}
+impl Post {
+    pub fn new() -> Post {
+        Post {
+            state: Some(Box::new(Draft {})),
+            content: String::new(),
+        }
+    }
 
-impl Screen {
-    pub fn run(&self) {
-        for component in self.components.iter() {
-            component.draw();
+    pub fn add_text(&mut self, text: &str) {
+        self.content.push_str(text);
+    }
+
+    pub fn content(&self) -> &str {
+        self.state.as_ref().unwrap().content(self)
+    }
+
+    pub fn request_review(&mut self) {
+        if let Some(s) = self.state.take() {
+            self.state = Some(s.request_review())
+        }
+    }
+
+    pub fn approve(&mut self) {
+        if let Some(s) = self.state.take() {
+            self.state = Some(s.approve())
         }
     }
 }
 
-pub struct Button {
-    pub width: u32,
-    pub height: u32,
-    pub label: String,
-}
+trait State {
+    // --snip--
+    fn request_review(self: Box<Self>) -> Box<dyn State>;
+    fn approve(self: Box<Self>) -> Box<dyn State>;
 
-impl Draw for Button {
-    fn draw(&self) {
-        // code to actually draw a button
+    fn content<'a>(&self, post: &'a Post) -> &'a str {
+        ""
     }
 }
 
-struct SelectBox {
-    width: u32,
-    height: u32,
-    options: Vec<String>,
-}
+// --snip--
 
-impl Draw for SelectBox {
-    fn draw(&self) {
-        // code to actually draw a select box
+struct Draft {}
+
+impl State for Draft {
+    fn request_review(self: Box<Self>) -> Box<dyn State> {
+        Box::new(PendingReview {})
+    }
+
+    fn approve(self: Box<Self>) -> Box<dyn State> {
+        self
     }
 }
 
+struct PendingReview {}
 
-fn main() {
-    let screen = Screen {
-        components: vec![
-            Box::new(SelectBox {
-                width: 75,
-                height: 10,
-                options: vec![
-                    String::from("Yes"),
-                    String::from("Maybe"),
-                    String::from("No"),
-                ],
-            }),
-            Box::new(Button {
-                width: 50,
-                height: 10,
-                label: String::from("OK"),
-            }),
-        ],
-    };
+impl State for PendingReview {
+    fn request_review(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
 
-    screen.run();
+    fn approve(self: Box<Self>) -> Box<dyn State> {
+        Box::new(Published {})
+    }
+}
+
+struct Published {}
+
+impl State for Published {
+    // --snip--
+    fn request_review(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+
+    fn approve(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+
+    fn content<'a>(&self, post: &'a Post) -> &'a str {
+        &post.content
+    }
+}
 }
