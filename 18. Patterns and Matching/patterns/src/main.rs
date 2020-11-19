e73d69f9556657c0ffba4f74bff99b19a0e92ce0
GuilhermeVBeira <guilherme.vieira.beira@gmail.com>
2020-11-19 12:21:47 -0300

    Matching Ranges of Values with ..=

--- 18. Patterns and Matching/patterns/src/main.rs
+++ 18. Patterns and Matching/patterns/src/main.rs
@@ -1,9 +1,8 @@
 fn main() {
-    let x = 1;
+    let x = 5;
 
     match x {
-        1 | 2 => println!("one or two"),
-        3 => println!("three"),
-        _ => println!("anything"),
+        1..=5 => println!("one through five"),
+        _ => println!("something else"),
     }
 }
