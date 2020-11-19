f0a523345b5a892848d554ce9e3bade15fee7b23
GuilhermeVBeira <guilherme.vieira.beira@gmail.com>
2020-11-19 12:15:47 -0300

    Matching Named Variables

--- 18. Patterns and Matching/patterns/src/main.rs
+++ 18. Patterns and Matching/patterns/src/main.rs
@@ -1,10 +1,12 @@
 fn main() {
-    let x = 1;
+    let x = Some(5);
+    let y = 10;
 
     match x {
-        1 => println!("one"),
-        2 => println!("two"),
-        3 => println!("three"),
-        _ => println!("anything"),
+        Some(50) => println!("Got 50"),
+        Some(y) => println!("Matched, y = {:?}", y),
+        _ => println!("Default case, x = {:?}", x),
     }
+
+    println!("at the end: x = {:?}, y = {:?}", x, y);
 }
