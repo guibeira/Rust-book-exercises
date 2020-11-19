5629ed99276206c698a61aab625cdb5121ac9b3c
GuilhermeVBeira <guilherme.vieira.beira@gmail.com>
2020-11-19 12:10:20 -0300

    Matching Literals

--- 18. Patterns and Matching/patterns/src/main.rs
+++ 18. Patterns and Matching/patterns/src/main.rs
@@ -1,5 +1,10 @@
 fn main() {
-    if let x = 5 {
-        println!("{}", x);
-    };
+    let x = 1;
+
+    match x {
+        1 => println!("one"),
+        2 => println!("two"),
+        3 => println!("three"),
+        _ => println!("anything"),
+    }
 }
