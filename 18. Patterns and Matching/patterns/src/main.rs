1260c0ca91425aceb9f78951842e2dc586d8b025
GuilhermeVBeira <guilherme.vieira.beira@gmail.com>
2020-11-19 14:27:27 -0300

    Destructuring to Break Apart Values

--- 18. Patterns and Matching/patterns/src/main.rs
+++ 18. Patterns and Matching/patterns/src/main.rs
@@ -1,8 +1,14 @@
-fn main() {
-    let x = 5;
+struct Point {
+    x: i32,
+    y: i32,
+}
 
-    match x {
-        1..=5 => println!("one through five"),
-        _ => println!("something else"),
+fn main() {
+    let p = Point { x: 0, y: 7 };
+
+    match p {
+        Point { x, y: 0 } => println!("On the x axis at {}", x),
+        Point { x: 0, y } => println!("On the y axis at {}", y),
+        Point { x, y } => println!("On neither axis: ({}, {})", x, y),
     }
 }
