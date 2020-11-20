748f0b24c5d6b9a5c079e9f2a96dc856f2c1d338
GuilhermeVBeira <guilherme.vieira.beira@gmail.com>
2020-11-19 22:54:38 -0300

    @ Bindings

--- 18. Patterns and Matching/patterns/src/main.rs
+++ 18. Patterns and Matching/patterns/src/main.rs
@@ -1,14 +1,17 @@
-struct Point {
-    x: i32,
-    y: i32,
-}
-
 fn main() {
-    let p = Point { x: 0, y: 7 };
+    enum Message {
+        Hello { id: i32 },
+    }
 
-    match p {
-        Point { x, y: 0 } => println!("On the x axis at {}", x),
-        Point { x: 0, y } => println!("On the y axis at {}", y),
-        Point { x, y } => println!("On neither axis: ({}, {})", x, y),
+    let msg = Message::Hello { id: 5 };
+
+    match msg {
+        Message::Hello {
+            id: id_variable @ 3..=7,
+        } => println!("Found an id in range: {}", id_variable),
+        Message::Hello { id: 10..=12 } => {
+            println!("Found an id in another range")
+        }
+        Message::Hello { id } => println!("Found some other id: {}", id),
     }
 }
