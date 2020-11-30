bffc3f7b62176c24839add034a7d14234785ae6a
GuilhermeVBeira <guilherme.vieira.beira@gmail.com>
2020-11-30 08:42:04 -0300

    Using Supertraits

--- 19. Advanced Features/trait.rs
+++ 19. Advanced Features/trait.rs
@@ -1,25 +1,31 @@
-use std::ops::Add;
+trait OutlinePrint: fmt::Display {
+    fn outline_print(&self) {
+        let output = self.to_string();
+        let len = output.len();
+        println!("{}", "*".repeat(len + 4));
+        println!("*{}*", " ".repeat(len + 2));
+        println!("* {} *", output);
+        println!("*{}*", " ".repeat(len + 2));
+        println!("{}", "*".repeat(len + 4));
+    }
+}
 
-#[derive(Debug, PartialEq)]
 struct Point {
     x: i32,
     y: i32,
 }
 
-impl Add for Point {
-    type Output = Point;
+impl OutlinePrint for Point {}
 
-    fn add(self, other: Point) -> Point {
-        Point {
-            x: self.x + other.x,
-            y: self.y + other.y,
-        }
+use std::fmt;
+
+impl fmt::Display for Point {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "({}, {})", self.x, self.y)
     }
 }
 
 fn main() {
-    assert_eq!(
-        Point { x: 1, y: 0 } + Point { x: 2, y: 3 },
-        Point { x: 3, y: 3 }
-    );
+    let p = Point { x: 1, y: 3 };
+    p.outline_print();
 }
