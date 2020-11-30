d3a60454f27b47ed8189cade7d6722e76c086f41
GuilhermeVBeira <guilherme.vieira.beira@gmail.com>
2020-11-30 20:01:27 -0300

    Dynamically Sized Types and the Sized Trait

--- 19. Advanced Features/advance.rs
+++ 19. Advanced Features/advance.rs
@@ -1,15 +1,3 @@
-
-fn main() {
-    type Thunk = Box<dyn Fn() + Send + 'static>;
-
-    let f: Thunk = Box::new(|| println!("hi"));
-
-    fn takes_long_type(f: Thunk) {
-        // --snip--
-    }
-
-    fn returns_long_type() -> Thunk {
-        // --snip--
-        Box::new(|| ())
-    }
+fn generic<T: ?Sized>(t: &T) {
+    // --snip--
 }
