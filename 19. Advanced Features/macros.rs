63be3c4d06509eae3a29f6c8d1a650308136f479
GuilhermeVBeira <guilherme.vieira.beira@gmail.com>
2020-12-01 07:38:27 -0300

    Declare macros (Meta Programming)

---
+++ 19. Advanced Features/macros.rs
@@ -0,0 +1,16 @@
+
+#![allow(unused)]
+fn main() {
+#[macro_export]
+macro_rules! vec {
+    ( $( $x:expr ),* ) => {
+        {
+            let mut temp_vec = Vec::new();
+            $(
+                temp_vec.push($x);
+            )*
+            temp_vec
+        }
+    };
+}
+}
