03cd46dfd8f82c2a4e9c44066de458766749cdfb
GuilhermeVBeira <guilherme.vieira.beira@gmail.com>
2020-12-05 14:25:00 -0300

    Use HTML on response

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -1,27 +1,30 @@
+use std::fs;
 use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
 
-
-fn main(){
+fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
 
-    for stream in listener.incoming(){
+    for stream in listener.incoming() {
         let stream = stream.unwrap();
 
         handle_connection(stream);
     }
-    
 }
 
-fn handle_connection(mut stream: TcpStream){
+fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 1024];
-
     stream.read(&mut buffer).unwrap();
-    
-    let response = "HTTP/1.1 200 OK\r\n\r\n";
+
+    let contents = fs::read_to_string("hello.html").unwrap();
+
+    let response = format!(
+        "HTTP/1.1 200 OK\r\nConntent-Length: {}\r\n\r\n{}",
+        contents.len(),
+        contents
+    );
 
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
-
 }
