60fbae543d83f519c7c2353111695b76d091ddd4
GuilhermeVBeira <guilherme.vieira.beira@gmail.com>
2020-12-05 14:37:00 -0300

    Check request URI

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -17,14 +17,17 @@ fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 1024];
     stream.read(&mut buffer).unwrap();
 
-    let contents = fs::read_to_string("hello.html").unwrap();
+    let get = b"GET / HTTP/1.1\r\n";
 
-    let response = format!(
-        "HTTP/1.1 200 OK\r\nConntent-Length: {}\r\n\r\n{}",
-        contents.len(),
-        contents
-    );
+    let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK\r\n\r\n", "hello.html") 
+    }else {
+        ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html") 
+    };
 
+    let contents = fs::read_to_string(filename).unwrap();
+    let response = format!("{}{}", status_line, contents);
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
+
 }
